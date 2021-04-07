6c93c36e29cd88ecacf70b16e08988f450c1c98d
Rainer Ramos <rainermr89@hotmail.com>
2021-04-07 11:41:24 -0300

    Primeira alteração

---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Contador de Pessoas",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Pessoas: 0",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "Pode Entrar",
+          style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0),
+        )
+      ],
+    ),
+  ));
+}
